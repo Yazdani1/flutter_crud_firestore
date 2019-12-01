658ebfdbbc4601ab991a612af333a1571023f306
Yazdani <yaz4dani@gmail.com>
2019-12-01 23:50:58 +0600

    Material Design

--- lib/FirestoreService.dart
+++ lib/FirestoreService.dart
@@ -14,30 +14,23 @@ class FirestoreService {
   factory FirestoreService(){
     return _firestoreService;
   }
-
   Stream<List<Note>> getNotes() {
     return _db.collection("note").orderBy("title", descending: true).snapshots().map((snapshot) =>
         snapshot.documents.map((doc) =>
             Note.fromMap(doc.data, doc.documentID)).toList(),
     );
   }
-
   //add note
 
   Future<void> addNote(Note note){
     return _db.collection("note").add(note.toMap());
   }
-
   //delete note
-
   Future<void> deleteNote(String id){
     return _db.collection("note").document(id).delete();
   }
-
   //update data
-
   Future<void>updateNote(Note note){
     return _db.collection("note").document(note.id).updateData(note.toMap());
   }
-
 }
